7134e2a532148bbdb92d54be52b6405ba42d942e
Seria <li12130@test.com>
2019-11-13 15:48:07 +0800

    配置antd和less

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,14 @@
-const { override } = require("customize-cra");
+const { override, addLessLoader, fixBabelImports } = require("customize-cra");
+const theme = require("./lessVar");
 
-module.exports = override();
+module.exports = override(
+    addLessLoader({
+        javascriptEnabled: true,
+    }),
+    fixBabelImports("import",{
+        libraryName:'antd',
+        libraryDirectory: 'es',
+        style:true,
+        modifyVars:theme
+    })
+);
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 
+import { Button } from "antd";
+
 class App extends Component {
     render() {
         return (
             <div>
-                App
+                <Button type="primary">App</Button>
             </div>
         );
     }
